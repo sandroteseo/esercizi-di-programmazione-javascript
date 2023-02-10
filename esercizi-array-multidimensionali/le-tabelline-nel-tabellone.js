5ab83fd3d5da4886b9159b5decb8d2b14b3eb56b
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-02-10 02:08:26 +0100

    Update le-tabelline-nel-tabellone.js

--- esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
+++ esercizi-array-multidimensionali/le-tabelline-nel-tabellone.js
@@ -11,7 +11,7 @@
     
   http://www.imparareaprogrammare.it
 */
-var tabellone=['x', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var tabellone=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 tabellone[0]=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 tabellone[1]=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 tabellone[2]=[0, 2, 4, 6, 8 , 10, 12, 14, 16, 18, 20];

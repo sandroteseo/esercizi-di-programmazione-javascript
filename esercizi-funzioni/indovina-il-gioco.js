8c283ca4bd63d90a658ee8910c9bff0262c391dc
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-02-14 14:54:15 +0100

    Update indovina-il-gioco.js

--- esercizi-funzioni/indovina-il-gioco.js
+++ esercizi-funzioni/indovina-il-gioco.js
@@ -14,3 +14,46 @@
 
   http://www.imparareaprogrammare.it
 */
+var n=6;
+
+function numbers (integer) {
+  if (integer>=1 && integer<=7) {
+    return integer;
+  } else {
+    return 'FALSE';
+  }
+}
+
+var num=numbers(n);
+
+function week (day) {
+  switch(day) {
+      
+    case 1:
+      console.log('1: Lunedì');
+      break;
+    case 2:
+      console.log('2: Martedì');
+      break;
+    case 3:
+      console.log('3: Mercoledì');
+      break;
+    case 4:
+      console.log('4: Giovedì');
+      break;
+    case 5:
+      console.log('5: Venerdì');
+      break;
+    case 6:
+      console.log('6: Sabato');
+      break;
+    case 7:
+      console.log('7: Domenica');
+      break;
+    default:
+      console.log('Peccato, non posso indovinare il giorno.');
+      
+    }
+}
+
+week(num);

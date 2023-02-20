8f9e19a83209afd065a31f72e15c8ff5c21faa1c
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-02-20 03:47:29 +0100

    Update chi-la-azzecca.js

--- esercizi-condizioni/chi-la-azzecca.js
+++ esercizi-condizioni/chi-la-azzecca.js
@@ -19,3 +19,22 @@
 
   http://www.imparareaprogrammare.it
 */
+var firstgamer = 10;
+var secondgamer = 5;
+var rand = Math.floor((Math.random() * (100-1) + 1));
+var difone = Math.abs(rand - firstgamer);
+var diftwo = Math.abs(rand - secondgamer);
+if (firstgamer==rand || secondgamer==rand) {
+   console.log('Vittoria!!!');
+} else {
+      if (difone < diftwo) {
+        console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 1 si è avvicinato di più!');
+      }
+      if (diftwo < difone) {
+        console.log('Nessuno dei due ha azzeccato il numero casuale, ma il giocatore 2 si è avvicinato di più!');
+      }
+      if (difone==diftwo) {
+        console.log('Nessuno dei due ha azzeccato il numero casuale!');
+      }
+
+} 

fe050d2a15bfdf0e0d0202a3b6a68c2df91ecbe0
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-02-21 11:42:04 +0100

    Update che-giorno-oggi.js

--- esercizi-condizioni/che-giorno-oggi.js
+++ esercizi-condizioni/che-giorno-oggi.js
@@ -22,4 +22,32 @@
 
   http://www.imparareaprogrammare.it
 */
-var day = 5;
+var day = Math.floor(Math.random() * (10 - 1) + 1);
+
+switch(day) {
+      
+  case 1:
+    console.log('Lunedì');
+    break;
+  case 2:
+    console.log('Martedì');
+    break;
+  case 3:
+    console.log('Mercoledì');
+    break;
+  case 4:
+    console.log('Giovedì');
+    break;
+  case 5:
+    console.log('Venerdì');
+    break;
+  case 6:
+    console.log('Sabato');
+    break;
+  case 7:
+    console.log('Domenica');
+    break;
+  default:
+    console.log('Errore! Dato non valido!');
+    
+  }

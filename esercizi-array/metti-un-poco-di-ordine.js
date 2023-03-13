d2dfed85601a326b43bf2de8cbed6fe755a138de
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-03-13 16:03:40 +0100

    Update metti-un-poco-di-ordine.js

--- esercizi-array/metti-un-poco-di-ordine.js
+++ esercizi-array/metti-un-poco-di-ordine.js
@@ -15,3 +15,30 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var array = [];
+for (var i = 0; i < 10; i++) {
+  array.push(Math.floor(Math.random() * 100 - 50));
+}
+
+// Stampa l'array originale
+console.log("Array originale: " + array);
+
+// Ordina l'array in modo decrescente usando il bubble sort
+var n = array.length;
+var swapped;
+do {
+  swapped = false;
+  for (var i = 0; i < n - 1; i++) {
+    if (array[i] < array[i + 1]) {
+      // Scambia gli elementi se il primo è minore del secondo
+      var temp = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = temp;
+      swapped = true;
+    }
+  }
+} while (swapped); // Ripeti finché ci sono scambi
+
+// Stampa l'array ordinato
+console.log("Array ordinato: " + array);

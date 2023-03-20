61704d1876540fb7ff7353e55b5896d7575bb112
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-03-21 00:43:02 +0100

    Update righe-o-colonne.js

--- esercizi-array-multidimensionali/righe-o-colonne.js
+++ esercizi-array-multidimensionali/righe-o-colonne.js
@@ -21,3 +21,34 @@
 
   http://www.imparareaprogrammare.it
 */
+var n=2, m=3;
+var matrix = [];
+var R = [];
+var C = [];
+
+for (var i=0; i<n; i++) {
+  var array = [];
+  for (var j=0; j<m; j++) {
+    var nums = Math.floor(Math.random() * (100 - 1) + 1);
+    array.push(nums);
+  }
+  matrix.push(array);
+}
+
+console.log(matrix);
+
+for (i=0; i<n; i++) {
+  var row = matrix[i].reduce((acc, curr) => acc + curr);
+  R.push(row);
+  
+}
+
+console.log(R);
+
+  for (j=0; j<m; j++) {
+    var column = matrix[0][j] + matrix[1][j];
+    C.push(column);
+  }
+
+
+console.log(C); 

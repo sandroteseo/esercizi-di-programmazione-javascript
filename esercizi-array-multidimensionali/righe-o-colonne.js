8c1cd9d38a4b561a08eacdd84485d119736d6895
sandroteseo <104635237+sandroteseo@users.noreply.github.com>
2023-03-21 01:05:16 +0100

    Update righe-o-colonne.js

--- esercizi-array-multidimensionali/righe-o-colonne.js
+++ esercizi-array-multidimensionali/righe-o-colonne.js
@@ -21,34 +21,39 @@
 
   http://www.imparareaprogrammare.it
 */
-var n=2, m=3;
-var matrix = [];
+// Prende in input due numeri, N e M.
+var N = 2;
+var M = 3;
+
+// Genera una matrice di NxM popolata di valori casuali compresi tra 1 e 100.
+var matrice = [];
+for (var i = 0; i < N; i++) {
+  var riga = [];
+  for (var j = 0; j < M; j++) {
+    var valore = Math.floor(Math.random() * 100) + 1;
+    riga.push(valore);
+  }
+  matrice.push(riga);
+}
+
+// Dichiara due array, l'array R di N elementi e l'array C di M elementi.
 var R = [];
 var C = [];
 
-for (var i=0; i<n; i++) {
-  var array = [];
-  for (var j=0; j<m; j++) {
-    var nums = Math.floor(Math.random() * (100 - 1) + 1);
-    array.push(nums);
+// Salva, nell'array R le somme di ogni riga della matrice e nell'array C le somme di tutte le colonne.
+for (var i = 0; i < N; i++) {
+  var sommaRiga = 0;
+  for (var j = 0; j < M; j++) {
+    sommaRiga += matrice[i][j];
+    if (i == 0) { // Inizializza l'array C con zeri
+      C.push(0);
+    }
+    C[j] += matrice[i][j]; // Aggiorna l'array C con la somma delle colonne
   }
-  matrix.push(array);
+  R.push(sommaRiga); // Aggiunge l'array R la somma della riga corrente
 }
 
-console.log(matrix);
-
-for (i=0; i<n; i++) {
-  var row = matrix[i].reduce((acc, curr) => acc + curr);
-  R.push(row);
-  
-}
-
-console.log(R);
-
-  for (j=0; j<m; j++) {
-    var column = matrix[0][j] + matrix[1][j];
-    C.push(column);
-  }
-
-
-console.log(C); 
+// Stampa la matrice e i due array.
+console.log("matrice =", matrice);
+console.log("array R =", R);
+console.log("array C =", C);
